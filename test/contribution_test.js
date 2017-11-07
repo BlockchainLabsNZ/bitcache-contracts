77ea5edde248fbcebe5ec1abdb843902e601a912
Klaus Hott Vidal <klahott@gmail.com>
2017-11-07 15:35:57 +1300

    Adding empty testFile

---
+++ test/contribution_test.js
@@ -0,0 +1,5 @@
+const assert = require("chai").assert;
+const BigNumber = web3.BigNumber;
+import { expectThrow, duration, latestBlock, getTime } from "./utils.js";
+
+contract("Contribution", ([miner, owner, investor, collector]) => {});
